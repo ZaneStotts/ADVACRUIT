bcc3045f4efd289c31d115fa25797ab0e49099a2
ZaneStotts <ZaneStotts@users.noreply.github.com>
2025-06-26 05:25:42 -0500

    Update header

--- src/header-javascript.php
+++ src/header-javascript.php
@@ -1,5 +1,8 @@
 const iconOpenMenu = document.getElementById("icon-open-menu");
 
+const headerLinksContainer = document.querySelector(".header-links-container");
+
 iconOpenMenu.addEventListener("click", () => {
+    headerLinksContainer.classList.toggle("open");
     iconOpenMenu.classList.toggle("open");
 })
