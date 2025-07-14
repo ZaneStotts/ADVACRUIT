bcb424b19f816b25c0c65f3150d4a9cacf1ac56a
ZaneStotts <ZaneStotts@users.noreply.github.com>
2025-07-14 05:56:49 -0500

    Update main

--- src/index.php
+++ src/index.php
@@ -19,7 +19,7 @@ if (isset($_SERVER['HTTP_X_FORWARDED_URL']) && strpos($_SERVER['HTTP_X_FORWARDED
     <!-- Link to Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&family=Varela+Round&family=Zen+Dots&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Varela+Round&family=Zen+Dots&display=swap" rel="stylesheet">
     <!-- Link to Font Awesome -->
     <script src="https://kit.fontawesome.com/619bfd8718.js" crossorigin="anonymous"></script>
   </head>
