2bf56374db8cca25d6caf7e2c8d57c353e8eb835
ZaneStotts <ZaneStotts@users.noreply.github.com>
2025-09-18 03:27:37 -0500

    Update blog

--- src/blog/blog-html.php
+++ src/blog/blog-html.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8" lang="en">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Blog | Advacruit</title>
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="../Advacruit_favicon.png">
     </head>
     <body>
     </body>
