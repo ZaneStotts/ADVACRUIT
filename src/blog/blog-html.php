5b44afd5fd8352d0853bca57fe858a62de432873
ZaneStotts <ZaneStotts@users.noreply.github.com>
2025-10-19 05:56:54 -0500

    Update blog

--- src/blog/blog-html.php
+++ src/blog/blog-html.php
@@ -37,19 +37,159 @@
                     <h3 class="article-subtitle">
                         Take a closer look at how the job searching process has changed in modern times and why junior roles are disappearing.
                     </h3>
-                    <h4>
-                        Read time: <span class="font-weight-normal">13 minutes</span>
-                    </h4>
-                    <h4 id="author">
-                        Author: <span class="font-weight-normal">Zane Stotts</span>
-                    </h4>
-                    <h4>
-                        Date: <span class="font-weight-normal">October 2, 2025</span>
-                    </h4>
+                    <div class="article-information">
+                        <ul class="article-information-icon-list">
+                            <li>
+                                <i class="fa-solid fa-user"></i>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-calendar-days"></i>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-stopwatch"></i>
+                            </li>
+                        </ul>
+                        <ul class="article-information-text-list">
+                            <li>
+                                <b>
+                                    Author:
+                                </b>
+                                     Zane Stotts
+                            </li>
+                            <li>
+                                <b>
+                                    Date:
+                                </b>
+                                 October 2, 2025
+                            </li>
+                            <li>
+                                <b>
+                                    Read time:
+                                </b>
+                                 13 minutes
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <article>
-                    The current hiring environment is incredibly bleak. There is not another way to describe it.
-                    <br>
+                    <h4>
+                        Introduction
+                    </h4>
+                    <p>
+                        The current hiring environment is incredibly bleak. There is not another way to describe it.
+                    </p>
+                    <p>
+                        Any average job seeker today is aware of this grim reality. Browsing online job boards for hours every week. Indeed, ZipRecruiter, LinkedIn, Dice, Monster—it doesn't matter. They all seem to blur together at a certain point. The same desolate mural painted with different colors.
+                    </p>
+                    <p>
+                        My story might sound very familiar for some. Raised by a software engineer father, I was always encouraged to pursue a career in technology. "Computers are never going to go away," he told me. And he was correct, of course. So it made sense to me to start studying web development in 2023.
+                    </p>
+                    <p>
+                        After completing my first online course in front end programming, I was ignorant enough to think I was prepared for the field. My father was proud of me and the validation made me believe I was ready to begin applying for jobs. During the following months, I endlessly prowled job postings and got nowhere.
+                    </p>
+                    <p>
+                        It didn't stop me. I continued my education and received six more certifications from two different institutions. Learned five coding languages. I launched three websites in an attempt to build a portfolio and show off my accumulated skills to potential employers. Revised my resume more times than I could keep track of. I was naive enough to think hard work pays off.
+                    </p>
+                    <p>
+                        I tried to follow all the advice I was given. Tailor my resumes in the hopes I could break through the ATS barricades. Be more deliberate in my job search by applying to only positions I knew I was qualified for. Research businesses and follow up with them. Be active and make connections with recruiters on LinkedIn. Try to build a network through family members and friends. Searching for work became a whole new job in itself, occasionally more exhausting than the one I already have.
+                    </p>
+                    <p>
+                        Now, two years later, I am still exactly where I was before. As I write this piece, I sit on a stack of 841 documented job applications. Out of which I have only secured three interviews, all leading to nothing.
+                    </p>
+                    <p>
+                        My unfruitful struggle to begin a new career slowly pushed me into a desperate spiral. I found myself applying to simply any role I thought I had a chance at. Banks, coffee houses, clothing stores, insurance companies, car shops, hotels, construction, and landscaping. Even positions requiring me to take significant pay cuts as well as jobs with long commute times.
+                    </p>
+                    <p>
+                        Only to receive the same generic rejection emails mocking me over and over again. "Unfortunately, your application was not selected at this time."
+                    </p>
+                    <p>
+                        What happened to all the supposed entry level jobs? Where were they? With seven years of experience in the work force, how could it be I was unqualified for literally everything?
+                    </p>
+                    <p>
+                        I realized there were several different aspects to the problem and it wasn't just me. I decided to take a deeper dive into the challenges facing job seekers today and why the current job market is so arduous. I was able to narrow it down to six key reasons, not necessarily in order.
+                    </p>
+                    <h4>
+                        #1. Refusal to train
+                    </h4>
+                    <p>
+                        Companies don't want to invest in their employees any longer. Teaching a new hire the correct approaches in a situation or how to excel in the position is now viewed as a disadvantage since it means spending time, money, and effort. Why provide support for staff when it's not really necessary?
+                    </p>
+                    <p>
+                        By eliminating the expense of training, businesses believe they will generate more revenue. Which might prove to be true in the short term, but can cause dangerous problems in the future. Placing heavier burdens on teams and straining resources to save dollars for the current quarter is not a sustainable solution. It risks manufacturing a product of lesser quality, resentful employees, alienating consumers, and a potentially tarnished reputation.
+                    </p>
+                    <p>
+                        Because of this recent trend, most employers are only focused on highly experienced applicants but are still unwilling to pay fair wages for their knowledgeable credentials. As job seekers grow increasingly desperate, companies have the ability to present lower and lower compensation rates, knowing the candidate will not have many options.
+                    </p>
+                    <p>
+                        I can personally attest to the myriad of job descriptions in the web development field asking for unreasonable requirements. I have seen examples of job descriptions including a master's degree but only paying a $50,000 salary. Or junior roles asking for eight years of experience. I'm certain there are many ridiculous instances existing in other industries as well.
+                    </p>
+                    <h4>
+                        #2. Jobs being offshored
+                    </h4>
+                    <p>
+                        In employers' constant conquest for cheaper labor, they have begun transferring a high number of entry level positions to other countries. India, the Philippines, and Mexico all serve as a highly popular destination for these jobs, where the cost of living is significantly lower. This means businesses can hire even more people for half as much money as an American candidate would need.
+                    </p>
+                    <p>
+                        Offshoring roles sounds like a dream for owners and CEOs but also poses a considerable threat to a large portion of the American working class. The positions getting moved across the ocean are heavily affecting the web development and software engineering fields, along with other various office jobs and customer service roles. But white collar jobs are not the only ones being endangered.
+                    </p>
+                    <p>
+                        Gathering from my almost two years of experience at National Oilwell Varco, the metal casting facility I worked at faced a huge dip in orders due to offshoring. I recall days when there would hardly be enough work to last a whole shift. Even times when I would sweep the ground for hours or cast unneeded parts just to appear busy.
+                    </p>
+                    <p>
+                        The plant manager finally revealed why we stopped receiving orders. They were being sent to India instead. Talk of layoffs also started circulating through departments. Even rumors of shutting down the facility entirely. Thankfully, I was able to land another role before the hammer eventually came down.
+                    </p>
+                    <p>
+                        My story goes to show how the escalation of offshore jobs is bad news for all sectors of American enterprise. According to a Forbes article<sup><a href="https://www.forbes.com/sites/jackkelly/2024/10/15/the-globalization-and-offshoring-of-us-jobs-have-hit-americans-hard/">1</a></sup>, an estimated 300,000 positions are being reassigned to outside nations every year. As long as businesses continue to repeatedly outsource their junior roles, it raises an imposing question for the average American job seeker—how will I earn enough to survive?
+                    </p>
+                    <h4>
+                        #3. Advancement of AI
+                    </h4>
+                    <p>
+                        Artificial intelligence has officially landed. It has been shoved in the face of the general public to the point it's basically inescapable. Companies are insistent on incorporating AI into all parts of daily life, from online search engines to vehicles to kitchen appliances. Every business now aggressively advertises their respective AI descendants, perpetually proclaiming its efficiency, precision, and superiority.
+                    </p>
+                    <p>
+                        While there is no doubt AI has helped humanity achieve impressive accomplishments, it has irrevocably complicated the job searching process, which was already challenging enough on its own. But for entry level job seekers, the rise of artificial intelligence could spell utter doom.
+                    </p>
+                    <p>
+                        Technology fields are obviously taking the hardest hits. According to a TechCrunch article<sup><a href="https://techcrunch.com/2025/05/15/programmers-bore-the-brunt-of-microsofts-layoffs-in-its-home-state-as-ai-writes-up-to-30-of-its-code/">2</a></sup>, Microsoft recently cut around 6,000 positions, majority of which were software programmers. The company shared AI now generates thirty percent of their code. In another piece<sup><a href="https://www.hrgrapevine.com/us/content/article/2024-08-08-dell-lays-off-12500-employees-to-become-leaner-in-shift-toward-ai?utm_source=search-list&utm_medium=search&utm_campaign=layoffs">3</a></sup> from HR Grapevine, Dell announced a mass layoff of 12,500 employees with the intention of a higher focus on artificial intelligence. Google also claims twenty-five percent of their code is composed by AI, as reported in a Fortune article<sup><a href="https://fortune.com/2024/10/30/googles-code-ai-sundar-pichai/">4</a></sup>.
+                    </p>
+                    <p>
+                        Even industries outside of tech are suffering. According to a Tech.co piece<sup><a href="https://tech.co/news/entry-level-roles-at-risk-due-to-ai-study">5</a></sup>, the great artificial intelligence surge is also making obsolete other entry level roles in administration, customer service, and accounting. The same source also reported the employment rate for people in their early twenties—between twenty-two and twenty-five—has fallen by thirteen percent in the last three years.
+                    </p>
+                    <p>
+                        Aneesh Raman, who is the chief economic opportunity officer at LinkedIn, recently spoke up about this rapidly expanding issue in a Fortune article<sup><a href="https://fortune.com/2025/05/25/ai-entry-level-jobs-gen-z-careers-young-workers-linkedin/">6</a></sup>. "While the technology sector is feeling the first wave of change, reflecting AI's mass adoption in this field, the erosion of entry level tasks is expected to play out in fields like finance, travel, food, and professional services too."
+                    </p>
+                    <p>
+                        Another quote from him seems to perfectly encapsulate the conflict facing most job seekers today because of artificial intelligence. "Breaking first is the bottom rung of the career ladder."
+                    </p>
+                    <h4>
+                        #4. Ghost jobs
+                    </h4>
+                    <p>
+                        With nearly all the steps of job applications now being handled through online boards, an additional struggle has emerged. Ghost jobs are positions posted by employers with no intention of hiring. And there are way more of them than people might think.
+                    </p>
+                    <p>
+                    </p>
+                    <p>
+                    </p>
+                    <p>
+                    </p>
+                    <p>
+                    </p>
+                    <h4>
+                        #5. Economic uncertainty
+                    </h4>
+                    <p>
+                    </p>
+                    <p>
+                    </p>
+                    <p>
+                    </p>
+                    <p>
+                    </p>
+                    <h4>
+                        #6. Ineffective hiring practices
+                    </h4>
                 </article>
             </section>
         </main>
